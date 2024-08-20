fa4e345307676ae1314647bfd3419be9157a4d20
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-08-20 08:39:22 -0600

    Practica2

--- practica2/src/index.ts
+++ practica2/src/index.ts
@@ -11,3 +11,22 @@ booleano = false;
 
 let encendido:boolean;
 //console.log(encendido);
+encendido = true;
+
+console.log('Encendido es:',encendido);
+console.log('Encendido es:', { encendido });
+
+encendido = true && false;
+encendido = true && true;
+encendido = false && true;
+encendido = false && false;
+
+console.log('Resultado:',encendido);
+
+let normal:any = 'hola';
+normal = 10;
+normal = true;
+normal = 11.25;
+console.log('Normal:',normal);
+
+
