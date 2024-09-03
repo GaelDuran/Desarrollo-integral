5447350de4c520c16b26b853c2b98681e0debbc6
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-09-03 08:41:51 -0600

    Practica 5

---
+++ practica5/src/index.ts
@@ -0,0 +1,24 @@
+let miVariable: string = 'Hola mundo';
+console.log(miVariable);
+
+const person1 = {
+    firstName1: "Jonh",
+    lastName1: "Doe",
+    age: 50
+};
+
+let {firstName1, lastName1} = person1;
+console.log(firstName1, ' ', lastName1);
+
+let miNombre = "Gael";
+let [a1, a2, a3, a4, a5]:any = miNombre;
+console.log(a1,a2,a3,a4,a5);
+console.log(a3);
+
+const frutas = ["platanos", "naranjas", "manzanas", "mangos"];
+let [fruta1, fruta2] = frutas;
+console.log(fruta1, fruta2);
+
+let [frutaA,,,frutaB] = frutas;
+console.log(frutaA, fruta2);
+
