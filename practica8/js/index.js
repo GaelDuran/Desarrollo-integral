e296f2bbc447b7e8781a78401bc03da407b237fa
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-09-24 08:22:47 -0600

    .

--- practica8/js/index.js
+++ practica8/js/index.js
@@ -1,2 +1,32 @@
 "use strict";
 console.log("Hola codespace");
+const nuevoHeroe = 'Spider-Man';
+function returnName() {
+    return nuevoHeroe;
+}
+const heroeNombre = returnName();
+let otraVariable = returnName();
+console.log('De que tipo es:', typeof otraVariable);
+//Parametros obligatorios en funciones, SWIFT
+const nombreCompleto = (nombre, apellido) => {
+    return `${nombre} ${apellido}`;
+};
+const tuNombre = nombreCompleto('Gael', 'Durán');
+console.log(tuNombre);
+//Parametros opcionales en funciones
+const nombreCompleto2 = (nombre, apellido) => {
+    return `${nombre || 'no se proporciono nombre'} ${apellido || 'no se proporciono apellido'}`;
+};
+const tuNombre2 = nombreCompleto2("Gael2");
+console.log(tuNombre2);
+const nombreCompleto3 = (nombre, apellido, mayusculas = false) => {
+    return mayusculas ? `${nombre || 'no se proporciono nombre'} ${apellido || 'no se proporciono apellido'}`.toUpperCase() : `${nombre || 'no se proporciono nombre'} ${apellido || 'no se proporciono apellido'}`;
+};
+const tuNombre3 = nombreCompleto3("Gael3", "Durán3", true);
+console.log(tuNombre3);
+// Operador REST
+const nombreCompleto4 = (nombre, edad, ...restoApellidos) => {
+    return `${nombre} ${restoApellidos.join(' ')} ${edad} años`;
+};
+const superman4 = nombreCompleto4('Clark', 32, 'Kent', 'Oswald', 'Smith', 'Otro', 'Uno mas');
+console.log(superman4);
--- practica8/src/index.ts
+++ practica8/src/index.ts
@@ -1,1 +1,48 @@
 console.log("Hola codespace")
+
+const nuevoHeroe:string = 'Spider-Man';
+
+function returnName():string{
+    return nuevoHeroe;
+}
+
+const heroeNombre = returnName();
+let otraVariable = returnName();
+console.log('De que tipo es:', typeof otraVariable);
+
+//Parametros obligatorios en funciones, SWIFT
+const nombreCompleto = (nombre:string, apellido:string) => {
+    return `${nombre} ${apellido}`;
+}
+
+const tuNombre = nombreCompleto('Gael', 'Durán');
+console.log(tuNombre);
+
+//Parametros opcionales en funciones
+const nombreCompleto2 = (nombre:string, apellido?:string) => {
+    return `${nombre || 'no se proporciono nombre'} ${apellido || 'no se proporciono apellido'}`;
+}
+
+const tuNombre2 = nombreCompleto2("Gael2");
+console.log(tuNombre2);
+
+const nombreCompleto3 = (nombre:string, apellido?:string, mayusculas:boolean = false) => {
+
+    return mayusculas ? `${nombre || 'no se proporciono nombre'} ${apellido || 'no se proporciono apellido'}`.toUpperCase() : `${nombre || 'no se proporciono nombre'} ${apellido || 'no se proporciono apellido'}`;
+}
+
+const tuNombre3 = nombreCompleto3("Gael3", "Durán3", true);
+console.log(tuNombre3);
+
+// Operador REST
+
+const nombreCompleto4 = (nombre:string, edad:number, ...restoApellidos:string[]) => {
+    return `${nombre} ${restoApellidos.join(' ')} ${edad} años`;
+
+}
+
+const superman4 = nombreCompleto4('Clark',32,'Kent','Oswald','Smith','Otro','Uno mas');
+console.log(superman4);
+
+
+
