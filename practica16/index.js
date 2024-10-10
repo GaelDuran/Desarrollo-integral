2b27e50b5a1e457a120efe5fe368f18a9c225c9f
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-10-09 19:36:38 -0600

    .

--- practica16/index.js
+++ practica16/index.js
@@ -24,8 +24,8 @@ const server = http.createServer((req, res) => {
             ruta += '/services.html';
             res.statusCode = 200;
             break;
-        case '/contact':
-            ruta += '/contact.html';
+        case '/contacto':
+            ruta += '/contacto.html';
             res.statusCode = 200;
             break;
         default:
@@ -34,41 +34,43 @@ const server = http.createServer((req, res) => {
             break;
     }
 
+    // Leer el archivo y responder con el contenido
     fs.readFile(ruta, (err, data) => {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
             return res.end('404 Not Found');
         }
 
+        // Escribe el HTML de navegación
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(`
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Mi Sitio</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link" href="/">Inicio</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/about">Acerca de</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/products">Productos</a>  <!-- Enlace corregido -->
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/services">Servicios</a>  <!-- Enlace corregido -->
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/contact">Contacto</a>
-            </li>
-        </ul>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Mi Sitio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="/">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">Acerca de</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/products">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/services">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contacto</a>
+        </li>
+      </ul>
     </div>
-    </nav>
+  </div>
+</nav>
         `);
         res.write(data);
         return res.end();
